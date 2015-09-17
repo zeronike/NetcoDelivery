269064997759320cead5f19c2b6493bac2ed8eff
Tuan Hoang <Tuanhla@Tuans-MacBook-Pro.local>
2015-09-17 10:05:48 +0700

    Initial Commit

---
+++ NetcoDelivery/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NetcoDelivery
+//
+//  Created by Tuan Hoang on 9/17/15.
+//  Copyright (c) 2015 Digital Systems. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
